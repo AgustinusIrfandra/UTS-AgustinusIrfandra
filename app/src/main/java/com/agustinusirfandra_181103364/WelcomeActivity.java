1dede84878b9e2efdac7c17b5be83dc28fd5b658
AgustinusIrfandra <agustinusirfandra.com>
2022-04-25 21:55:36 +0700

    first commit

---
+++ app/src/main/java/com/agustinusirfandra_181103364/WelcomeActivity.java
@@ -0,0 +1,14 @@
+package com.agustinusirfandra_181103364;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
